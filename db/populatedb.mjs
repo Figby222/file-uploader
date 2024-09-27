d357bd79caafaaa0eb51f4fe8fdb28765aaf3bc0
Figby222 <figby222@outlook.com>
2024-09-27 09:10:31 -0700

    Add populatedb.mjs

---
+++ db/populatedb.mjs
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+import { genPasswordHash } from "../lib/passwordHashingUtils.mjs";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.user.createManyAndReturn([
+        { username: "cookies@cookies.com", password: await genPasswordHash("cookies") },
+        { username: "sandwich@sandwich.com", password: await genPasswordHash("sandwich") },
+        { username: "sandwich@cookies.com", password: await genPasswordHash("sandwichCookies") },
+        { username: "cookies@sandwich.com", password: await genPasswordHash("cookiesSandwich") },
+        { username: "Ryan@me.com", password: await genPasswordHash("RyanMe") }
+    ])
+}
+
+main();
