836aea292410ee5194408e5c6aeeaf4ca12099fb
Figby222 <figby222@outlook.com>
2024-09-28 04:01:23 -0700

    Wrap created users in object with data property

--- db/populatedb.mjs
+++ db/populatedb.mjs
@@ -4,13 +4,15 @@ import { genPasswordHash } from "../lib/passwordHashingUtils.mjs";
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.user.createManyAndReturn([
-        { username: "cookies@cookies.com", password: await genPasswordHash("cookies") },
-        { username: "sandwich@sandwich.com", password: await genPasswordHash("sandwich") },
-        { username: "sandwich@cookies.com", password: await genPasswordHash("sandwichCookies") },
-        { username: "cookies@sandwich.com", password: await genPasswordHash("cookiesSandwich") },
-        { username: "Ryan@me.com", password: await genPasswordHash("RyanMe") }
-    ])
+    await prisma.user.createManyAndReturn({
+        data: [
+            { username: "cookies@cookies.com", password: await genPasswordHash("cookies") },
+            { username: "sandwich@sandwich.com", password: await genPasswordHash("sandwich") },
+            { username: "sandwich@cookies.com", password: await genPasswordHash("sandwichCookies") },
+            { username: "cookies@sandwich.com", password: await genPasswordHash("cookiesSandwich") },
+            { username: "Ryan@me.com", password: await genPasswordHash("RyanMe") }
+        ]
+    })
 }
 
 main();
