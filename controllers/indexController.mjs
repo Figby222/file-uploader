46853aa0b4e9b066c12f56056cd06c3c76357afd
Figby222 <figby222@outlook.com>
2024-09-28 04:26:01 -0700

    Add signUpPost() controller

--- controllers/indexController.mjs
+++ controllers/indexController.mjs
@@ -1,3 +1,27 @@
+import asyncHandler from "express-async-handler";
+import { body, validationResult } from "express-validator";
+import db from "../db/queries/userQueries.mjs";
+import { genPasswordHash } from "../lib/passwordHashingUtils.mjs";
+
+const validateUserSignUp = [
+    body("username")
+        .trim()
+        .notEmpty().withMessage("Username must not be empty")
+        .isEmail().withMessage("Username must be formatted like an email ex: example@example.com")
+        .isLength({ max: 255 }).withMessage("Username must contain a maximum of 255 characters")
+        .custom(async (username) => {
+            const userExists = !!await db.findUserByUsername(username);
+            return userExists;
+        }).withMessage("Username already taken"),
+    body("password")
+        .notEmpty().withMessage("Password must not be empty")
+        .isLength({ min: 6, max: 50 }).withMessage("Password must contain between 6 & 50 characters"),
+    body("confirm_password")
+        .custom((confirmPassword, { req }) => {
+            return confirmPassword === req.body.password;
+        }).withMessage("Confirm Password must be the same as Password")
+]
+
 function indexRouteGet(req, res) {
     res.render("index", { title: "Node Template" });
 }
@@ -6,4 +30,20 @@ function signUpFormGet(req, res) {
     res.render("sign-up-form");
 }
 
-export { indexRouteGet, signUpFormGet };
+const signUpPost = [
+    validateUserSignUp,
+    asyncHandler(async (req, res) => {
+        const errorsResult = validationResult(req);
+        if (!errorsResult.isEmpty()) {
+            res.render("sign-up-form", { username: req.body.username, errors: errorsResult.errors });
+            return;
+        }
+        const hashedPassword = await genPasswordHash(req.body.password);
+        await db.createUser({
+            username: req.body.username,
+            password: hashedPassword
+        })
+        res.redirect("/log-in");
+    }) 
+]
+export { indexRouteGet, signUpFormGet, signUpPost };
