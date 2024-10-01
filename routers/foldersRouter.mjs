00743fc0a027ab93683516eb859f7e8ae7e91c2a
Figby222 <figby222@outlook.com>
2024-09-30 21:23:46 -0700

    Move create folder route up

--- routers/foldersRouter.mjs
+++ routers/foldersRouter.mjs
@@ -3,8 +3,9 @@ import * as foldersController from "../controllers/foldersController.mjs";
 
 const foldersRouter = Router({ mergeParams: true });
 
+foldersRouter.post("/:folderId/create-folder", foldersController.createFolderPost);
+
 foldersRouter.get("/:folderId", foldersController.getFiles);
 
-foldersRouter.post("/:folderId/create-folder", foldersController.createFolderPost);
 
 export default foldersRouter;
