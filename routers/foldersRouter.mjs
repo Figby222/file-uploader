97ce630ce56a01925029342cc77c2edcbf0ff8ad
Figby222 <figby222@outlook.com>
2024-09-30 21:31:15 -0700

    Add /rename/:folderId GET route

--- routers/foldersRouter.mjs
+++ routers/foldersRouter.mjs
@@ -3,9 +3,12 @@ import * as foldersController from "../controllers/foldersController.mjs";
 
 const foldersRouter = Router({ mergeParams: true });
 
+foldersRouter.get("/rename/:folderId", foldersController.renameFolderFormGet);
+
 foldersRouter.post("/:folderId/create-folder", foldersController.createFolderPost);
 
 foldersRouter.get("/:folderId", foldersController.getFiles);
+
 
 
 export default foldersRouter;
