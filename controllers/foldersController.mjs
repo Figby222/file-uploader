faf987a155f2cb5c3f3a0a8866acab3ae75dd954
Figby222 <figby222@outlook.com>
2024-09-30 20:03:04 -0700

    Add foldersController.mjs

---
+++ controllers/foldersController.mjs
@@ -0,0 +1,14 @@
+import authorizationUtils from "../lib/authorizationUtils.mjs";
+const { checkLoggedIn } = authorizationUtils;
+import db from "../db/queries/folderQueries.mjs";
+import asyncHandler from "express-async-handler";
+
+const getFiles = [
+    checkLoggedIn,
+    asyncHandler(async (req, res) => {
+        const folderContents = await db.getFiles(parseInt(req.params.folderId));
+        res.render("files-list", { files: folderContents.files, childFolders: folderContents.childFolders})
+    })
+]
+
+export { getFiles }
