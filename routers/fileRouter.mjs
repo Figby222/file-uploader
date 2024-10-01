f78332374e53348c193497bca3af2062ff65bfc9
Figby222 <figby222@outlook.com>
2024-09-30 23:32:54 -0700

    Add /upload/:folderId POST route

--- routers/fileRouter.mjs
+++ routers/fileRouter.mjs
@@ -12,6 +12,8 @@ fileRouter.get("/upload/:folderId", fileController.uploadFileFormGet);
 
 fileRouter.get("/upload", fileController.uploadFileFormGet);
 
+fileRouter.post("/upload/:folderId", fileController.uploadFilePost);
+
 fileRouter.post("/upload", fileController.uploadFilePost);
 
 export default fileRouter;
