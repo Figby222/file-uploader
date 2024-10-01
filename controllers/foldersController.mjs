d71891b9589a8eb8499c1d46455d78b8798e47ba
Figby222 <figby222@outlook.com>
2024-09-30 20:55:38 -0700

    Add createFolderPost() controller

--- controllers/foldersController.mjs
+++ controllers/foldersController.mjs
@@ -2,6 +2,15 @@ import authorizationUtils from "../lib/authorizationUtils.mjs";
 const { checkLoggedIn } = authorizationUtils;
 import db from "../db/queries/folderQueries.mjs";
 import asyncHandler from "express-async-handler";
+import { body, validationResult } from "express-validator";
+
+const validateNewFolder = [
+    body("folder_name")
+        .trim()
+        .notEmpty().withMessage("Folder Name must not be empty")
+        .isAlphanumeric("en-US", { ignore: /[-_]/i }).withMessage("Folder Name must be alphanumeric")
+        .isLength({ max: 50 }).withMessage("Folder Name must contain a maximum of 50 characters")
+]
 
 const getFiles = [
     checkLoggedIn,
@@ -11,4 +20,35 @@ const getFiles = [
     })
 ]
 
-export { getFiles }
+const createFolderPost = [
+    checkLoggedIn,
+    validateNewFolder,
+    asyncHandler(async (req, res) => {
+        const errorsResult = validationResult(req);
+        const parentFolderId = req.body.parentFolderId === 0 ? null : req.body.parentFolderId;
+        if (!errorsResult.isEmpty()) {
+            const folderContents = await db.getFiles(parentFolderId);
+            res.render("files-list", { 
+                errors: errorsResult.errors, 
+                currentFolderId: parentFolderId, 
+                files: folderContents.files, 
+                childFolders: folderContents.childFolders 
+            })
+            return;
+    
+        }
+    
+        await db.createFolder({
+            name: req.body.folder_name,
+            parentFolderId: parentFolderId
+        })
+        
+        const redirectLink = parentFolderId ? 
+            `/files/folders/${parentFolderId}` :
+            `/files`;
+            
+        res.redirect(redirectLink)
+    })
+]
+
+export { getFiles, createFolderPost }
