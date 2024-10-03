7a999ec31fdcab77575977b13f45a6bff9415a9b
Figby222 <figby222@outlook.com>
2024-10-02 21:20:44 -0700

    Add createSharedFolderPost() controller

--- controllers/sharedFolderController.mjs
+++ controllers/sharedFolderController.mjs
@@ -1,15 +1,24 @@
 import asyncHandler from "express-async-handler";
 import db from "../db/queries/sharedFolderQueries.mjs";
 import { NotFoundError } from "../lib/errorUtils.mjs";
+import authorizationUtils from "../lib/authorizationUtils.mjs";
+const { checkLoggedIn } = authorizationUtils;
 
 const sharedFolderGet = asyncHandler(async (req, res) => {
     const sharedFolderContents = await db.sharedFolderContentsGet(req.params.sharedFolderId);
-
     if (!sharedFolderContents) {
         throw new NotFoundError("Shared Folder Not Found");
     }
-
     res.render("files-list", { files: sharedFolderContents.folder.files})
 })
 
-export { sharedFolderGet }
+const createSharedFolderPost = [
+    checkLoggedIn,
+    asyncHandler(async (req, res) => {
+        const folderId = req.params.folderId ? parseInt(req.params.folderId) : null;
+        const sharedFolderDetails = await db.createSharedFolder(folderId);
+        res.redirect(`/files/share/${sharedFolderDetails.id}`);
+    })
+]
+
+export { sharedFolderGet, createSharedFolderPost }
