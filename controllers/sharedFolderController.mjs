5553cdb7ea85b4329171de2bcc869078a40dbc42
Figby222 <figby222@outlook.com>
2024-10-02 22:04:32 -0700

    Add form validation to createSharedFolderPost()

--- controllers/sharedFolderController.mjs
+++ controllers/sharedFolderController.mjs
@@ -2,7 +2,15 @@ import asyncHandler from "express-async-handler";
 import db from "../db/queries/sharedFolderQueries.mjs";
 import { NotFoundError } from "../lib/errorUtils.mjs";
 import authorizationUtils from "../lib/authorizationUtils.mjs";
-const { checkLoggedIn } = authorizationUtils;a
+const { checkLoggedIn } = authorizationUtils;
+import { body, validationResult } from "express-validator";
+import folderDb from "../db/queries/folderQueries.mjs";
+
+const validateCreateSharedFolder = [
+    body("share_folder_duration")
+        .notEmpty().withMessage("Duration must not be empty")
+        .isInt({ min: 0 }).withMessage("Duration must be a postive integer")
+]
 
 const sharedFolderGet = asyncHandler(async (req, res) => {
     const sharedFolderContents = await db.sharedFolderContentsGet(req.params.sharedFolderId);
@@ -14,9 +22,23 @@ const sharedFolderGet = asyncHandler(async (req, res) => {
 
 const createSharedFolderPost = [
     checkLoggedIn,
+    validateCreateSharedFolder,
     asyncHandler(async (req, res) => {
+        const errorsResult = validationResult(req);
         const folderId = req.params.folderId ? parseInt(req.params.folderId) : null;
-        const sharedFolderDetails = await db.createSharedFolder(folderId);
+        if (!errorsResult.isEmpty()) {
+            const folderContents = await folderDb.getFiles(folderId);
+            res.render("files-list", {
+                shareFolderErrors: errorsResult.errors,
+                currentFolderId: folderId,
+                files: folderContents.files,
+                childFolders: folderContents.childFolders,
+            })
+            return;
+        }
+        const expiresAt = new Date(Date.now() + parseInt(req.body.share_folder_duration))
+            .toISOString();
+        const sharedFolderDetails = await db.createSharedFolder(folderId, { expiresAt: expiresAt });
         res.redirect(`/files/share/${sharedFolderDetails.id}`);
     })
 ]
