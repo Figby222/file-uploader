9f8c44cad0f27a53176b9c111da54d2638d6eb49
Figby222 <figby222@outlook.com>
2024-10-02 21:02:09 -0700

    Used sharedFolderRouter on /share

--- routers/fileRouter.mjs
+++ routers/fileRouter.mjs
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import * as fileController from "../controllers/fileController.mjs";
 import foldersRouter from "./foldersRouter.mjs";
+import sharedFolderRouter from "./sharedFolderRouter.mjs";
 
 const fileRouter = Router();
 
 fileRouter.use("/folders", foldersRouter);
+
+fileRouter.use("/share", sharedFolderRouter);
 
 fileRouter.get("/", fileController.filesListGet);
 
@@ -15,10 +18,7 @@ fileRouter.get("/upload", fileController.uploadFileFormGet);
 fileRouter.get("/download/:fileId", fileController.downloadFileGet);
 
 fileRouter.get("/:fileId", fileController.fileDetailsGet);
-
 fileRouter.post("/upload/:folderId", fileController.uploadFilePost);
-
 fileRouter.post("/upload", fileController.uploadFilePost);
-
 
 export default fileRouter;
